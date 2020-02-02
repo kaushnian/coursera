c95f069c08ae2715da9c6215854a932465d7a16b
Yevgeny Kaushnian <kefbox@gmail.com>
2020-02-02 16:49:27 -0500

    Fix fib huge

--- algorithms/1/week2_algorithmic_warmup/5_fibonacci_number_again/fibonacci_huge.js
+++ algorithms/1/week2_algorithmic_warmup/5_fibonacci_number_again/fibonacci_huge.js
@@ -2,26 +2,48 @@
 
 const readline = require('readline');
 const rl = readline.createInterface({
-    input: process.stdin,
-    terminal: false
+  input: process.stdin,
+  terminal: false
 });
 
 process.stdin.setEncoding('utf8');
 rl.on('line', readLine);
 
 function readLine(line) {
-    if (line !== "\n") {
-        const n = parseInt(line.toString().split(' ')[0], 10);
-        const m = parseInt(line.toString().split(' ')[1], 10);
+  if (line !== '\n') {
+    const n = parseInt(line.toString().split(' ')[0], 10);
+    const m = parseInt(line.toString().split(' ')[1], 10);
 
-        console.log(getFibMod(n, m));
-        process.exit();
-    }
+    console.log(getFibMod(n, m));
+    process.exit();
+  }
 }
 
+// Fi mod m = ?
 
-function getFibMod(n, m) {
-    // write your code here
+// 2015 mod 8(length) = 7, 7 mod 3 = 1;
+
+function getFibMod(i, m) {
+  if (i === 0) return 0;
+  if (i === 1) return 1;
+
+  const fi = [0, 1];
+  let period = [0, 1];
+  const pattern = '0,1';
+  let calcPeriod = true;
+
+  let index = 2;
+
+  while (calcPeriod) {
+    fi[index] = fi[index - 1] + fi[index - 2];
+    period.push(fi[index] % m);
+    index++;
+    calcPeriod = period.slice(-2).join() !== pattern;
+  }
+
+  period = period.slice(0, -2);
+
+  return fi[i % period.length] % m;
 }
 
 module.exports = getFibMod;
